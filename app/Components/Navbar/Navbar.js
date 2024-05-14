7b205d93fef4ec45a4f3885804bd7af241f4c7f6
basant-elsobky <124135940+basant-elsobky@users.noreply.github.com>
2024-05-15 01:44:26 +0300

    Resolve conflicts

--- app/Components/Navbar/Navbar.js
+++ app/Components/Navbar/Navbar.js
@@ -29,7 +29,7 @@ function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg p-3">
             <div className="container-fluid">
-                <img src='/images/logo.png' />
+                <img src='/Images/logo.png' />
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
